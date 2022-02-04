f3a7286f5ce6dbf93a09e5c950d4100e4c3deece
Daniil Kuznetsov <ldan93@yandex.ru>
2022-02-04 19:59:27 +0300

    add allure report

--- src/test/java/in/reqres/Specifications.java
+++ src/test/java/in/reqres/Specifications.java
@@ -1,5 +1,6 @@
 package in.reqres;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -13,7 +14,8 @@ public class Specifications {
         return new RequestSpecBuilder()
                 .setBaseUri(url)
                 .setContentType(ContentType.JSON)
-                .build();
+                .build()
+                .filter(new AllureRestAssured());
     }
 
     public static ResponseSpecification responseSpec200() {
